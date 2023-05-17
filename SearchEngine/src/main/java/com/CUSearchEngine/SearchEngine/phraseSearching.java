bd089e942140867c1ae3820fdbe529f3de38def6
sohad-hossam <sohad95husam@gmail.com>
2023-05-18 02:16:29 +0300

    PhraseSearcher_Part2

--- SearchEngine/src/main/java/com/CUSearchEngine/SearchEngine/phraseSearching.java
+++ SearchEngine/src/main/java/com/CUSearchEngine/SearchEngine/phraseSearching.java
@@ -1,20 +1,12 @@
 package com.CUSearchEngine.SearchEngine;
-
-import org.jsoup.Jsoup;
-
 import java.io.IOException;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import java.util.ArrayList;
 import java.util.List;
 
-
-import static java.lang.Math.log;
 
 
 public class phraseSearching {
@@ -34,45 +26,43 @@ public class phraseSearching {
             }
         }
         //iterating on the urls
+
         for (int j = 0; j < Data.get(minIndex).data.size(); j++)
         {
-           // long time1=System.currentTimeMillis();
-            String currentUrl = Data.get(minIndex).data.get(j).URL;
-            HttpClient client = HttpClients.createDefault();
-            HttpGet request = new HttpGet(currentUrl);
-            HttpResponse response = client.execute(request);
-            HttpEntity entity = response.getEntity();
-            String html = EntityUtils.toString(entity);
 
-            //String body = html.body().text().toLowerCase();
-            //String head = html.head().text().toLowerCase();
+            String currentUrl = Data.get(minIndex).data.get(j).URL;
+            Document doc = Jsoup.connect(currentUrl).get();
+            String body = doc.body().text().toLowerCase();
+            String head = doc.head().text().toLowerCase();
 
             int wordCount = 0;
-            if  (html.contains(orginalPhrase))
+            if  (body.contains(orginalPhrase))
             {
-                int index = html.indexOf(orginalPhrase);
+                int index = body.indexOf(orginalPhrase);
                 while (index != -1) {
                     wordCount++;
-                    index = html.indexOf(orginalPhrase, index + orginalPhrase.length());
+                    index = body.indexOf(orginalPhrase, index + orginalPhrase.length());
                 }
             }
-//            if (html.contains(orginalPhrase))
-//            {
-//                int index = html.indexOf(orginalPhrase);
-//                while (index != -1) {
-//                    wordCount++;
-//                    index = head.indexOf(orginalPhrase, index + orginalPhrase.length());
-//                }
-//            }
+            if (head.contains(orginalPhrase))
+            {
+                int index = head.indexOf(orginalPhrase);
+                while (index != -1) {
+                    wordCount++;
+                    index = head.indexOf(orginalPhrase, index + orginalPhrase.length());
+                }
+            }
             if(wordCount != 0) {
                 Website phrase = new Website();
-                phrase.TF = (double) wordCount / (double) html.length();
+                phrase.TF = (double) wordCount / (double) head.length();
                 phrase.title = Data.get(minIndex).data.get(j).title;
                 phrase.URL = Data.get(minIndex).data.get(j).URL;
                 phrase.paragraph = Data.get(minIndex).data.get(j).paragraph;
                 UrlsDataReturned.add(phrase);
             }
+
         }
+
 
         return UrlsDataReturned;
     }
